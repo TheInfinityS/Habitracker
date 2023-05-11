975547333e28505f57c2068e2779481917efe189
TheInfinityS <infinityitf@gmail.com>
2023-05-11 21:03:00 +0600

    Add dependencies

---
+++ src/test/java/com/intership/app/habittracker/HabittrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intership.app.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabittrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
