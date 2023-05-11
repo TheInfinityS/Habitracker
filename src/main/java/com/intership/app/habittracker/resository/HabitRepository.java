aa71b8fe42fe9637d888e962fef6a12980f06136
TheInfinityS <infinityitf@gmail.com>
2023-05-11 21:11:30 +0600

    Add repoositories

---
+++ src/main/java/com/intership/app/habittracker/resository/HabitRepository.java
@@ -0,0 +1,13 @@
+package com.intership.app.habittracker.resository;
+
+import com.intership.app.habittracker.entity.Habit;
+import com.intership.app.habittracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HabitRepository extends JpaRepository<Habit,Long> {
+    List<Habit> findByProfile(User profile);
+}
